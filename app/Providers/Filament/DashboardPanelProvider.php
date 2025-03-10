2b70b351d24396c65bca1ba72ea298762a432d42
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-03-10 01:01:24 -0600

    Añadiendo el DashboardPanelProvider y registrándolo en los proveedores de la aplicación

---
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Pages;
+use Filament\Panel;
+use Filament\Widgets;
+use Filament\PanelProvider;
+use Filament\Navigation\MenuItem;
+use Filament\Support\Colors\Color;
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Filament\Http\Middleware\AuthenticateSession;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+
+class DashboardPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('dashboard')
+            ->default()
+            ->path('dashboard')
+            ->login()
+            ->profile(isSimple: false)
+            ->brandName('Reservaciones TecNM - Ciudad Valles')
+            ->favicon(asset('img/logo.ico'))
+            ->userMenuItems([
+                'profile' => MenuItem::make()->label('Editar perfil')->icon('heroicon-o-user'),
+                'logout' => MenuItem::make()->label('Cerrar sesión')->icon('heroicon-o-arrow-left-start-on-rectangle'),
+            ])
+            ->spa()
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->discoverResources(in: app_path('Filament/Dashboard/Resources'), for: 'App\\Filament\\Dashboard\\Resources')
+            ->discoverPages(in: app_path('Filament/Dashboard/Pages'), for: 'App\\Filament\\Dashboard\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Dashboard/Widgets'), for: 'App\\Filament\\Dashboard\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
